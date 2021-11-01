184a02df8d6584fe001ea71da0dc13ff78b3ddfe
rhl120 <rhl120@protonmail.com>
2021-11-01 10:07:32 +0100

    Put windows where they belong

--- config.h
+++ config.h
@@ -31,7 +31,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Zathura",  NULL,	  NULL,       1 << 3,	    0,	         -1 },
+	{ "mpv",      NULL,      NULL,	      1 << 4,       1,	         -1 },
+	{ "qutebrowser",  NULL,       NULL,   1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,11 +64,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lib_command[] = { "/home/rhl120/.local/bin/read_dox.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = lib_command } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -121,7 +125,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
-
+//could be concatenated into  1 string seprated by ;s but that wouldn't look good
 const uint8_t num_auto_cmds = 7;
 const char *cmds_auto_start[] = {
 	"xinput set-prop '/dev/wsmouse' 'WS Pointer Wheel Emulation' 1",
@@ -131,4 +135,6 @@ const char *cmds_auto_start[] = {
 	"xinput set-prop '/dev/wsmouse' 'WS Pointer Wheel Emulation Inertia' 3",
 	"xrandr --output LVDS-1 --off",
 	"/home/rhl120/.local/bin/dwmblocks &"
+	"qutebrowser&"
+	"st& st&"
 };
