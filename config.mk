52f094b6d73631ca27e91db083d155d7deae5784
RHL120 <rhl120@protonmail.com>
2024-07-10 20:11:47 +0200

    Use CFLAGS in config.mk

--- config.mk
+++ config.mk
@@ -26,10 +26,10 @@ INCS = -I${X11INC} -I${FREETYPEINC} `pkg-config --cflags x11 freetype2 xfont2`
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS += -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = `pkg-config --libs x11 freetype2 xft fontconfig xinerama`
+CFLAGS   += -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  += `pkg-config --libs x11 freetype2 xft fontconfig xinerama`
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
