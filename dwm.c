9701af4f57b320e71587ed7cffbc7c16cce1e65c
RHL120 <rhl120@protonmail.com>
2024-07-10 23:42:25 +0200

    Don't draw over the dock in monocle mode

--- dwm.c
+++ dwm.c
@@ -1125,6 +1125,7 @@ void
 monocle(Monitor *m)
 {
 	unsigned int n = 0;
+	unsigned int dockh = m->dock? m->dock->h : 0;
 	Client *c;
 
 	for (c = m->clients; c; c = c->next)
@@ -1133,7 +1134,7 @@ monocle(Monitor *m)
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw - dockh, 0);
 }
 
 void
