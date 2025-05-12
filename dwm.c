d9be5cb905309e25d00d2cde01cd6c064962fc5e
RHL120 <rhl120@protonmail.com>
2025-05-12 18:35:02 +0200

    Remove all information related to versioning

--- dwm.c
+++ dwm.c
@@ -2228,7 +2228,7 @@ void
 updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		*stext = 0;
 	drawbar(selmon);
 }
 
@@ -2409,10 +2409,6 @@ auto_start(void)
 int
 main(int argc, char *argv[])
 {
-	if (argc == 2 && !strcmp("-v", argv[1]))
-		die("dwm-"VERSION);
-	else if (argc != 1)
-		die("usage: dwm [-v]");
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
 		fputs("warning: no locale support\n", stderr);
 	if (!(dpy = XOpenDisplay(NULL)))
